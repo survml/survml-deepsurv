050a6efff2bc62df4525d3089fe0f114cbe86051
jiaxiang <jiaxiang002@e.ntu.edu.sg>
2023-01-04 14:49:42 +0800

    refined codes and added testing process right away after training

--- train.py
+++ train.py
@@ -1,5 +1,7 @@
+import random
 import uuid
 
+import numpy as np
 import torch
 import pandas as pd
 from matplotlib import pyplot as plt
@@ -12,8 +14,8 @@ sys.path.append(os.path.abspath(os.getcwd()))
 
 from dataset import get_time_status
 from model import DeepSurv, partial_ll_loss
-# from test import *
-from config import *
+from testing import test_step
+from config import DATASET, LR, BATCH_SIZE, TRAIN_EPOCHS
 
 
 def train_step(model, x, t, e, optimizer, bs=256, seed=100):
@@ -53,8 +55,23 @@ def train_step(model, x, t, e, optimizer, bs=256, seed=100):
     return epoch_loss / n
 
 
+def set_seed(seed: int = 42) -> None:
+    np.random.seed(seed)
+    random.seed(seed)
+    torch.manual_seed(seed)
+    # torch.cuda.manual_seed(seed)
+    # # When running on the CuDNN backend, two further options must be set
+    # torch.backends.cudnn.deterministic = True
+    # torch.backends.cudnn.benchmark = False
+    # Set a fixed value for the hash seed
+    os.environ["PYTHONHASHSEED"] = str(seed)
+    print("Random seed set as {}".format(seed))
+
+
 if __name__ == "__main__":
-    df = pd.read_csv('{}_parsed.csv'.format(DATASET))
+    set_seed()
+
+    df = pd.read_csv('data/{}_parsed.csv'.format(DATASET))
     time_name, status_name = get_time_status(DATASET)
 
     X = df.drop(columns=[status_name, time_name])
@@ -64,51 +81,48 @@ if __name__ == "__main__":
 
     # normalization for features
     X = (X - X.min()) / (X.max() - X.min())
-
-    X_train, _, y_train, _ = train_test_split(X, y, test_size=0.2, random_state=42)
-    # X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.25, random_state=42)
-
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
     t_train, e_train = y_train[time_name], y_train[status_name]
-    # t_val, e_val = y_val[time_name], y_val[status_name]
+    t_test, e_test = y_test[time_name], y_test[status_name]
 
     X_train, t_train, e_train = \
         torch.tensor(X_train.to_numpy()).float(), torch.tensor(t_train.to_numpy()).float(), \
         torch.tensor(e_train.to_numpy()).float()
-    # X_val, t_val, e_val = \
-    #     torch.tensor(X_val.to_numpy()).float(), torch.tensor(t_val.to_numpy()).float(), \
-    #     torch.tensor(e_val.to_numpy()).float()
+
+    X_test, t_test, e_test = \
+        torch.tensor(X_test.to_numpy()).float(), torch.tensor(t_test.to_numpy()).float(), \
+        torch.tensor(e_test.to_numpy()).float()
 
     model = DeepSurv(inputdim=input_dim, layers=[25, 25])
-
     optimizer = torch.optim.Adam(model.parameters(), lr=LR)
-    # optimizer = get_optimizer(model, lr)
 
     train_losses, val_losses, ci_curve = [], [], []
 
-    for epoch in range(1600):
-
-        # train_step_start = time.time()
+    for epoch in range(TRAIN_EPOCHS):
         train_loss = train_step(model, X_train, t_train, e_train, optimizer, BATCH_SIZE, seed=epoch)
-        # print(f'Duration of train-step: {time.time() - train_step_start}')
-        # test_step_start = time.time()
-        # val_loss, ci = test_step(model, X_val, t_val, e_val)
-        # print(f'Duration of test-step: {time.time() - test_step_start}')
-
         train_losses.append(train_loss)
-        # val_losses.append(val_loss)
-        # ci_curve.append(ci)
 
         if epoch % 10 == 0:
-            print("epoch:\t", epoch + 1,
-                  "\t train loss:\t", "%.6f" % round(train_loss, 6))
+            print("epoch:\t", epoch,
+                  "\t train loss:\t", "%.4f" % round(train_loss, 4))
 
     plt.plot(train_losses)
     plt.title("Average Train Loss")
     plt.xlabel('epochs')
     plt.ylabel('train loss')
-    plt.savefig("models/dsurv/img/dsurv_train_{}_lr={}_bs={}_time={}.png".format(DATASET, LR, BATCH_SIZE, uuid.uuid1()))
-    plt.show()
+    plt.savefig("img/train_{}_lr={}_bs={}_epoch={}_time={}.png"
+                .format(DATASET, LR, BATCH_SIZE, TRAIN_EPOCHS, uuid.uuid1()))
+    # plt.show()
 
-    torch.save(model.state_dict(),
-               'models/dsurv/saved_models/dsurv_{}_lr={}_bs={}_time={}.pt'
-               .format(DATASET, LR, BATCH_SIZE, uuid.uuid1()))
+    # save the trained model
+    model_name = 'DeepSurv_{}_lr={}_bs={}_time={}.pt'.format(DATASET, LR, BATCH_SIZE, uuid.uuid1())
+    torch.save(model.state_dict(), 'saved_models/{}'.format(model_name))
+
+    # test the saved model tight away
+    test_model = DeepSurv(inputdim=input_dim, layers=[25, 25])
+    test_model.load_state_dict(
+        torch.load('saved_models/{}'.format(model_name)))
+    test_model.eval()
+
+    _, ci = test_step(test_model, X_test, t_test, e_test)
+    print("Concordance Index:", round(ci, 4))
