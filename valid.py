6df2b4a151792cf53c9d7eaf47cc2fedf3e90eb5
jiaxiang <jiaxiang002@e.ntu.edu.sg>
2023-01-04 14:50:14 +0800

    added early stop for recommending training epochs

--- valid.py
+++ valid.py
@@ -1,6 +1,8 @@
 import uuid
 import numpy as np
 import pandas as pd
+import torch
+import random
 from matplotlib import pyplot as plt
 from sklearn.model_selection import train_test_split
 
@@ -10,13 +12,28 @@ sys.path.append(os.path.abspath(os.getcwd()))
 
 from dataset import get_time_status
 from model import DeepSurv
-from train import *
-from testing import *
-from config import *
+from train import train_step
+from testing import test_step
+from config import DATASET, LR, EPOCHS, BATCH_SIZE
+
+
+def set_seed(seed: int = 42) -> None:
+    np.random.seed(seed)
+    random.seed(seed)
+    torch.manual_seed(seed)
+    # torch.cuda.manual_seed(seed)
+    # # When running on the CuDNN backend, two further options must be set
+    # torch.backends.cudnn.deterministic = True
+    # torch.backends.cudnn.benchmark = False
+    # Set a fixed value for the hash seed
+    os.environ["PYTHONHASHSEED"] = str(seed)
+    print("Random seed set as {}".format(seed))
 
 
 if __name__ == "__main__":
-    df = pd.read_csv('{}_parsed.csv'.format(DATASET))
+    set_seed()
+
+    df = pd.read_csv('data/{}_parsed.csv'.format(DATASET))
     time_name, status_name = get_time_status(DATASET)
 
     X = df.drop(columns=[status_name, time_name])
@@ -36,6 +53,7 @@ if __name__ == "__main__":
     X_train, t_train, e_train = \
         torch.tensor(X_train.to_numpy()).float(), torch.tensor(t_train.to_numpy()).float(), \
         torch.tensor(e_train.to_numpy()).float()
+
     X_val, t_val, e_val = \
         torch.tensor(X_val.to_numpy()).float(), torch.tensor(t_val.to_numpy()).float(), \
         torch.tensor(e_val.to_numpy()).float()
@@ -46,6 +64,7 @@ if __name__ == "__main__":
     # optimizer = get_optimizer(model, lr)
 
     train_losses, val_losses, ci_curve = [], [], []
+    last_val_loss = 100
 
     for epoch in range(EPOCHS):
 
@@ -56,19 +75,23 @@ if __name__ == "__main__":
         val_loss, ci = test_step(model, X_val, t_val, e_val)
         # print(f'Duration of test-step: {time.time() - test_step_start}')
 
-        train_losses.append(train_loss)
-        val_losses.append(val_loss)
-        ci_curve.append(ci)
+        train_losses.append(train_loss), val_losses.append(val_loss), ci_curve.append(ci)
 
         if epoch % 10 == 0:
-            print("epoch:\t", epoch + 1,
-                  "\t train loss:\t", "%.6f" % round(train_loss, 6),
-                  "\t valid loss:\t", "%.6f" % round(val_loss, 6),
+            print("epoch:\t", epoch,
+                  "\t train loss:\t", "%.4f" % round(train_loss, 4),
+                  "\t valid loss:\t", "%.4f" % round(val_loss, 4),
                   "\t concordance index:\t", "%.4f" % round(ci, 4))
+            if val_loss > last_val_loss:
+                print("Early stop as validation loss rebounded!\n"
+                      "Recommended training epochs: {}".format(epoch))
+                break
+            else:
+                last_val_loss = val_loss
 
     fig, axs = plt.subplots(3, figsize=[15, 20])
     axs[0].plot(train_losses), axs[0].set_title("Average Train Loss")
     axs[1].plot(val_losses), axs[1].set_title("Average Validation Loss")
     axs[2].plot(ci_curve), axs[2].set_title("Concordance Index")
-    fig.savefig("models/dsurv/img/dsurv_valid_{}_lr={}_bs={}_time={}.png".format(DATASET, LR, BATCH_SIZE, uuid.uuid1()))
-    plt.show()
+    fig.savefig("img/valid_{}_lr={}_bs={}_epoch={}_time={}.png".format(DATASET, LR, BATCH_SIZE, epoch, uuid.uuid1()))
+    # plt.show()
